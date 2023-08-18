6655567cb2272c49a9a95ad505180b2a4884af79
Pedro Lucas Neves Lopes <111779550+Pedr0LnL@users.noreply.github.com>
2023-08-18 12:44:06 -0300

    Update Exercicio1.java

---
+++ atividade2/Exercicio1.java
@@ -0,0 +1,42 @@
+package atividade2;
+
+import java.util.Scanner;
+
+public class Exercicio1 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Digite o primeiro número: ");
+        double num1 = scanner.nextDouble();
+
+        System.out.print("Digite o segundo número: ");
+        double num2 = scanner.nextDouble();
+
+        System.out.print("Digite a operação (+, -, *, /): ");
+        char operation = scanner.next().charAt(0);
+
+        double result = 0.0;
+
+        switch (operation) {
+            case '+':
+                result = num1 + num2;
+                break;
+            case '-':
+                result = num1 - num2;
+                break;
+            case '*':
+                result = num1 * num2;
+                break;
+            case '/':
+                result = num1 / num2;
+                break;
+            default:
+                System.out.println("Operação inválida.");
+                
+        }
+
+        System.out.println("Resultado: " + result);
+
+        scanner.close();
+    }
+}
